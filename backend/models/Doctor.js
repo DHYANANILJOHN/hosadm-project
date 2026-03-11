f509700d2379423184a9170bad1b85bbd0dcc14b
DHYANANILJOHN <dhyanajohn@gmail.com>
2026-03-11 09:27:28 +0530

    update

--- backend/models/Doctor.js
+++ backend/models/Doctor.js
@@ -1,42 +1,45 @@
 const mongoose = require("mongoose");
 
-const doctorSchema = new mongoose.Schema({
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
 
-  name: {
-    type: String,
-    required: true
+    specialty: {
+      type: String,
+      required: true
+    },
+
+    experience: {
+      type: Number
+    },
+
+    phone: {
+      type: String,
+      match: /^[0-9]{10}$/   // only 10 digit numbers
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    status: {
+      type: String,
+      default: "Pending"
+    }
   },
-
-  specialty: {
-    type: String,
-    required: true
-  },
-
-  experience: {
-    type: Number
-  },
-
-  phone: {
-    type: String,
-    match: /^[0-9]{10}$/
-  },
-
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-
-  password: {
-    type: String,
-    required: true
-  },
-
-  status: {
-    type: String,
-    default: "Pending"
+  {
+    timestamps: true
   }
-
-});
+);
 
 module.exports = mongoose.model("Doctor", doctorSchema);
